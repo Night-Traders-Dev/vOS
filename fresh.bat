01a3fbd2f0d27b7dd7deae4a8538b8e8b4bbb66a
Night-Traders-Dev <quegmeister@gmail.com>
2024-05-03 16:14:52 -0400

    migrated vOS to src, added fresh.bat

--- fresh.bat
+++ fresh.bat
@@ -2,6 +2,6 @@
 echo "Removing old files...\n"
 rd /s /q src
 git clone https://github.com/Night-Traders-Dev/vOS.git
-mv vOS/src src
+move vOS/src src
 rd /s /q vOS
 echo "Fresh build installed in src/"
