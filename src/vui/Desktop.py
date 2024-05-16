69494d6d1ac76dab3778c0240d09c49bb936de55
Night-Traders-Dev <quegmeister@gmail.com>
2024-05-16 14:11:05 -0400

    Desktop UI added to vui

--- src/vui/Desktop.py
+++ src/vui/Desktop.py
@@ -1,31 +1,40 @@
 from textual.app import App, ComposeResult
 from textual.color import Color
-from textual.widgets import Static, Digits, Button
-from textual.widget import Widget
+from textual.widgets import Static
 from textual.geometry import Region
 from textual.screen import Screen
 from textual import on, events, work
 from datetime import datetime
 
+
 class DesktopBase(Screen):
 
     def compose(self) -> ComposeResult:
-        self.dash = Widget(id="dash")
+        self.dash = Static(id="dash", classes="DashClass")
         yield self.dash
         yield Static(id="topbar")
         yield Static("", id="clock")
+        yield Static(id="dashbutton", classes="DashClass")
 
     # Clock Method
     @on(events.Mount)
     def clock_timer(self) -> None:
+        self.dash_open = False
+        self.dash_timer = 0
         self.update_clock()
         self.set_interval(1, self.update_clock)
 
     def update_clock(self) -> None:
+        if self.dash_open:
+           if self.dash_timer != 3:
+              self.dash_timer += 1
+           else:
+              self.dash.styles.animate("opacity", value=0.0, duration=0.5)
+              self.dash_timer = 0
+              self.dash_open = False
         clock = datetime.now().time()
         self.query_one("#clock", Static).update(f"{clock:%T}")
     # End Clock
-
 
     # MouseMove Alternative
     def is_mouse_over_widget(self, widget_x, widget_y, widget_width, widget_height, mouse_x, mouse_y, screen_width, screen_height):
@@ -33,19 +42,21 @@ class DesktopBase(Screen):
 
     # Dash Reveal Trigger
     @on(events.MouseEvent)
-    def dash_trigger(self, event: events.MouseEvent) -> None:
+    def dash_thrigger(self, event: events.MouseEvent) -> None:
         dash_loc = self.dash.region
         term_height = 24
         term_width = 80
         if self.is_mouse_over_widget(dash_loc.x, dash_loc.y, dash_loc.width, dash_loc.height, event.x, event.y, term_width, term_height):
             if self.dash.opacity == 0.0:
+                self.dash_open = True
                 self.dash.styles.animate("opacity", value=100.0, duration=1.5)
             else:
                 pass
         else:
             self.dash.styles.animate("opacity", value=0.0, duration=0.5)
+            self.dash_timer = 0
+            self.dash_open = False
     # End Dash Reveal
-
 
 class Desktop(App):
     CSS_PATH = "ui.tcss"
