e33ce866e25f7bceb93584990a0bf0020120dd09
Night-Traders-Dev <quegmeister@gmail.com>
2024-05-16 12:58:23 -0400

    Desktop UI added to vui

--- src/vui/Desktop.py
+++ src/vui/Desktop.py
@@ -1,42 +1,11 @@
-from textual.app import App, ComposeResult, RenderResult
-from textual.renderables.gradient import LinearGradient
+from textual.app import App, ComposeResult
 from textual.color import Color
-from textual.widgets import Static, Digits, Button, Label
+from textual.widgets import Static, Digits, Button
 from textual.widget import Widget
-from textual.reactive import reactive
 from textual.geometry import Region
 from textual.screen import Screen
 from textual import on, events, work
 from datetime import datetime
-from math import sin, pi
-from time import time
-
-
-COLORS = [
-    "#2C001E",
-    "#3A052B",
-    "#481A38",
-    "#562445",
-    "#643052",
-    "#713A5F",
-    "#7F456C",
-    "#8D4F79",
-    "#9B5A86",
-    "#A96593",
-    "#77216F",
-]
-
-STOPS = [
-     (i / (len(COLORS) - 1), color) for i, color in enumerate(COLORS)
-]
-
-
-
-
-class AppDrawer(Screen):
-
-   def compose(self) -> ComposeResult:
-     yield Widget(id="AppDrawer")
 
 class DesktopBase(Screen):
 
@@ -46,7 +15,7 @@ class DesktopBase(Screen):
         yield Static(id="topbar")
         yield Static("", id="clock")
 
-    #Clock Method
+    # Clock Method
     @on(events.Mount)
     def clock_timer(self) -> None:
         self.update_clock()
@@ -55,12 +24,14 @@ class DesktopBase(Screen):
     def update_clock(self) -> None:
         clock = datetime.now().time()
         self.query_one("#clock", Static).update(f"{clock:%T}")
+    # End Clock
 
 
+    # MouseMove Alternative
     def is_mouse_over_widget(self, widget_x, widget_y, widget_width, widget_height, mouse_x, mouse_y, screen_width, screen_height):
         return widget_x <= mouse_x <= widget_x + widget_width and widget_y <= mouse_y <= widget_y + widget_height
 
-    # dash reveal trigger
+    # Dash Reveal Trigger
     @on(events.MouseEvent)
     def dash_trigger(self, event: events.MouseEvent) -> None:
         dash_loc = self.dash.region
@@ -73,11 +44,12 @@ class DesktopBase(Screen):
                 pass
         else:
             self.dash.styles.animate("opacity", value=0.0, duration=0.5)
+    # End Dash Reveal
+
 
 class Desktop(App):
     CSS_PATH = "ui.tcss"
-    SCREENS = {"DesktopBase": DesktopBase(), "AppDrawer": AppDrawer()}
-    MODES= {"DesktopBase": DesktopBase(), "AppDrawer": AppDrawer()}
+    SCREENS = {"DesktopBase": DesktopBase()}
 
     @work
     async def on_mount(self) -> None:
